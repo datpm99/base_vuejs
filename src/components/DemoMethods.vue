e4358d1a14a11b040bf8469fe716ab6d37e194c1
datpm <paindat9911@gmail.com>
2022-02-08 22:10:51 +0700

    first commit

---
+++ src/components/DemoMethods.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>Status: {{ statusFromId(status) }}</p>
+
+    <p v-for="(number, index) in numberPositive(numbers)" :key="index">
+      {{ number }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoMethods",
+  data() {
+    return {
+      status: 1,
+      numbers: [-1, 0, 5, 3, -6, 1],
+    };
+  },
+  methods: {
+    statusFromId(id) {
+      const status = {
+        0: "zero",
+        1: "one",
+        2: "two",
+      }[id];
+      return status || "Unknown status: " + id;
+    },
+
+    numberPositive(numbers) {
+      return numbers.filter((number) => number >= 0);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
